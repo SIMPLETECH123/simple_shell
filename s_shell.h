e06c6217dadd4e8d84936abbf2d932e054f6bcff
Timilehin Caleb <simpletimi100@gmail.com>
2023-04-17 17:35:51 -0700

    shell

--- s_shell.h
+++ s_shell.h
@@ -104,7 +104,8 @@ typedef struct passinfo
 } info_t;
 
 #define INFO_INIT \
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \ 0, 0, 0}
+{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
+	0, 0, 0}
 
 /**
  * struct builtin - func contains a builtin string
